26316f2b362203c6c38245794682bcc0ab57224e
sjodie1157 <sjodie1157@gmail.com>
2023-10-10 13:40:06 +0200

    added rate * days work formul

--- script'/code.js
+++ script'/code.js
@@ -1,7 +1,9 @@
 let hoursWorked = prompt ("enter your hours")
-console.log(`work hours was ${hoursWorked}`);
+console.log(`work hours was ${hoursWorked} hours`);
 
 
 let workRate = prompt ("Work rate")
 console.log(`Rate per day ${workRate}`);
 
+hoursWorked *= workRate
+console.log(`R ${hoursWorked}`);
